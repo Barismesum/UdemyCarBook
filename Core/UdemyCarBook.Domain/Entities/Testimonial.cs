74f1447ab773a31bddaf3f523bf7b9d4077c6411
barismesum <bmesum99@gmail.com>
2024-10-19 17:53:52 +0300

    entities migrated,persistance layer added,about cqrs added,about api added

--- Core/UdemyCarBook.Domain/Entities/Testimonial.cs
+++ Core/UdemyCarBook.Domain/Entities/Testimonial.cs
@@ -8,7 +8,7 @@ namespace UdemyCarBook.Domain.Entities
 {
     public class Testimonial
     {
-        public int TestimonailID { get; set; }
+        public int TestimonialID { get; set; }
         public string  Name { get; set; }
         public string Comment { get; set; }
         public string ImageUrl { get; set; }
